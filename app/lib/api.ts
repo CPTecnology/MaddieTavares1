083634bf931eef1298648a081d00ebe61342a9a9
crist <cjpires@uta.cv>
2026-02-02 11:58:51 -0100

    servidor concet

--- app/lib/api.ts
+++ app/lib/api.ts
@@ -1,21 +1,21 @@
 import axios from 'axios';
+
 export const runtime = 'edge';
+
 const api = axios.create({
-  // Usar 127.0.0.1 evita atrasos de DNS no Windows
-  baseURL: 'http://127.0.0.1:3333', 
+  // O NOVO ENDEREÇO SEGURO
+  baseURL: 'https://api.maddietavares.cv', 
   headers: {
     'Content-Type': 'application/json',
   },
-  timeout: 10000, // Se o servidor não responder em 10s, cancela (evita spinner infinito)
+  timeout: 10000, 
 });
 
 api.interceptors.request.use((config) => {
   const token = typeof window !== 'undefined' ? localStorage.getItem("token") : null;
-  
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  
   return config;
 }, (error) => {
   return Promise.reject(error);
